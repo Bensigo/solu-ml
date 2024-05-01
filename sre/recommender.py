1e0e8538b03ee7ce7ce74f8c0b1cc4cdc3a81894
bensigo <sigocode@gmail.com>
2024-05-01 17:39:03 +0400

    feat: add lifespan to server

--- sre/recommender.py
+++ sre/recommender.py
@@ -11,12 +11,17 @@ class Recommender:
         self.tfidf_matrix = self._compute_tfidf_matrix()
 
     def _compute_tfidf_matrix(self):
+        if len(self.data) == 0:
+            return None
         # Join the lists of tags into a single string for each article
         self.data['tags'] = self.data['tags'].apply(lambda x: ' '.join(x))
         return self.tfidf_vectorizer.fit_transform(self.data['tags'])
 
     def get_recommendations(self, input_tags, num_recommendations=10):
+        if len(self.data) == 0:
+            return None
         input_tags_str = ' '.join(input_tags)
+        print(input_tags_str)
         input_vec = self.tfidf_vectorizer.transform([input_tags_str])
         sim_scores = list(enumerate(cosine_similarity(input_vec, self.tfidf_matrix)[0]))
         sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
